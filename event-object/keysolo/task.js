f118595992e830aa3d236e5ba7ba14d9b5f7fc04
Georgiy <urumyanga@gmail.com>
2026-07-03 19:23:33 +0300

    first commit keysolo

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,15 +17,21 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода символа вызываем this.success()
-      При неправильном вводе символа - this.fail();
-      DOM-элемент текущего символа находится в свойстве this.currentSymbol.
-     */
-  }
+    document.addEventListener('keydown', (event) => {
+        const current = this.currentSymbol.textContent.toLowerCase();
+        const pressed = event.key.toLowerCase();
+
+        if (pressed.length !== 1) {
+            return;
+        }
+
+        if (current === pressed) {
+            this.success();
+        } else {
+            this.fail();
+        }
+    });
+}
 
   success() {
     if(this.currentSymbol.classList.contains("symbol_current")) this.currentSymbol.classList.remove("symbol_current");
